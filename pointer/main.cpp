5997a2fc9042bf78bb6d8830e7329360480b9fae
ciecclm <clm@imnu.edu.cn>
2015-11-11 20:10:00 +0800

    第二次提交
    printf变成2行了

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -10,6 +10,8 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    printf("1、Hello, World!\n");
+    printf("2、Hello, World!\n");
+
     return 0;
 }
